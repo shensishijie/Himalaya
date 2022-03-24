3ca94ae892ee367a4c1209495a334314f411a8f7
wujielong <@wujielong>
2022-03-24 18:45:28 +0800

    p27

---
+++ app/src/main/java/com/example/himalaya/adapters/DetailListAdapter.java
@@ -0,0 +1,76 @@
+package com.example.himalaya.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.himalaya.R;
+import com.ximalaya.ting.android.opensdk.model.track.Track;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailListAdapter extends RecyclerView.Adapter<DetailListAdapter.InnerHolder> {
+
+    private List<Track> mDetailData = new ArrayList<>();
+    private SimpleDateFormat mUpdateDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private SimpleDateFormat mDurationFormat = new SimpleDateFormat("mm:ss");
+
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_album_datail, parent, false);
+        return new InnerHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
+        //找到控件，设置数据
+        View itemView = holder.itemView;
+        //顺序Id
+        TextView orderTv = itemView.findViewById(R.id.order_text);
+        //标题
+        TextView titleTv = itemView.findViewById(R.id.detail_item_title);
+        //播放次数
+        TextView playCountTv = itemView.findViewById(R.id.detail_item_play_count);
+        //时长
+        TextView durationTv = itemView.findViewById(R.id.detail_item_duration);
+        //更新日期
+        TextView updateDateTv = itemView.findViewById(R.id.detail_item_update_time);
+
+        //设置数据
+        Track track = mDetailData.get(position);
+        orderTv.setText((position + 1) + "");
+        titleTv.setText(track.getTrackTitle());
+        playCountTv.setText(track.getPlayCount() + "");
+
+        String duration = mDurationFormat.format(track.getDuration() * 1000);
+        durationTv.setText(duration);
+
+        String updateTimeText = mUpdateDateFormat.format(track.getUpdatedAt());
+        updateDateTv.setText(updateTimeText);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDetailData.size();
+    }
+
+    public void setData(List<Track> trackList) {
+        mDetailData.clear();
+        mDetailData.addAll(trackList);
+        notifyDataSetChanged();
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
